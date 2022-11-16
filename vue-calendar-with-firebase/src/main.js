1a8bc413b601784e68c67ba8ba50873a1df0d596
Stefan Markov <stefan.t.markov@gmail.com>
2022-11-16 15:08:09 +0200

    Add some methods and components.

--- vue-calendar-with-firebase/src/main.js
+++ vue-calendar-with-firebase/src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_API_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
